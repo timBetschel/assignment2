7fc70e885a4cb9353d527da4d7ff6bd39626f0fd
tim <s3898252@student.rmit.edu.au>
2023-09-11 19:28:22 +1000

    init

---
+++ pcb.h
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "types.h"
+
+#pragma once  // tells the compiler to only include this file once
+              /**
+ * the process control block - represents a process that needs to be executed in
+ * our simulator. Please note the I used some custom types of osp2023::time_type
+ * and osp2023::id_type. these are just aliases to long integers but they are
+ * more meaningful type names, hinting at how they will be used. Please see
+ * their definitions in types.h.
+ **/
+class pcb {
+    // the unique process id
+    osp2023::id_type id;
+    // the total time that a process / job should run for
+    osp2023::time_type total_time;
+    // the time that has been used so far for this process
+    osp2023::time_type time_used;
+    // the priority level for this process
+    // how much time has this process spent waiting for the cpu?
+    osp2023::time_type total_wait_time;
+    // what time was this process last on the cpu?
+
+   public:
+    // max and min duration for a process in our system.
+    static constexpr osp2023::time_type MAX_DURATION = 100;
+    static constexpr osp2023::time_type MIN_DURATION = 10;
+};
---
+++ types.h
@@ -0,0 +1,9 @@
+#include <string>
+#pragma once
+namespace osp2023 {
+// renaming some types so that the types are more meaningful
+using time_type = long long;
+using id_type = int;
+// constant that can be used to initialise a time to not having been set
+extern constexpr time_type time_not_set = -1;
+}
